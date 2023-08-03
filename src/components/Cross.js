e236e832748f47d37a0bd9d8acf9cf9d773dd5ba
Lucifer1207 <harshsingh05839@gmail.com>
2023-08-03 18:15:05 +0530

    added

---
+++ src/components/Cross.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cross.css';
+
+function Cross() {
+  return (
+    <div>
+      <img src="cross.png" alt="im" />
+    </div>
+  )
+}
+
+export default Cross
---
+++ src/components/MenuIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Menu.css';
+
+function MenuIcon() {
+  return (
+    <div>
+      <img src="menu.png" alt="im" />
+    </div>
+  )
+}
+
+export default MenuIcon
